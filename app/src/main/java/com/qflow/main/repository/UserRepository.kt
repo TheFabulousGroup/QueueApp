ee9deee95ec23e0598fe01253aac51d1a32160d4
Rocío García <rocioganu96@gmail.com>
2020-03-07 22:57:25 +0100

    changes userRepository and LoginActivities

--- app/src/main/java/com/qflow/main/repository/UserRepository.kt
+++ app/src/main/java/com/qflow/main/repository/UserRepository.kt
@@ -1,18 +1,15 @@
 package com.qflow.main.repository
+import android.content.ContentValues.TAG
+import android.util.Log
 
-import com.google.android.gms.tasks.OnFailureListener
-import com.google.android.gms.tasks.OnSuccessListener
-import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.qflow.main.core.BaseRepository
 import com.qflow.main.core.Failure
 import com.qflow.main.domain.adapters.UserAdapter
 import com.qflow.main.domain.local.database.AppDatabase
 import com.qflow.main.domain.local.database.user.UserDB
-import com.qflow.main.domain.server.models.UserServerModel
 import com.qflow.main.usecases.Either
-
-
 
 
 /**
@@ -20,65 +17,81 @@ import com.qflow.main.usecases.Either
  * to the user
  * */
 interface UserRepository {
+
     fun saveUser(selectedPass: String, email: String): Either<Failure, Long>
+    fun signIn(email: String,pass:String)
+    fun currentUserauth(): FirebaseUser?
+    fun checkUser(): Either<Failure, Any>
+    fun signOut()
+
     class General
-    //class Local
     constructor(
+        private var auth: FirebaseAuth = FirebaseAuth.getInstance(),
         private val appDatabase: AppDatabase,
         val userAdapter: UserAdapter
-
     ) : BaseRepository(), UserRepository {
 
-
         override fun saveUser(
-            //username: String,
             selectedPass: String,
             selectedEmail: String
         ): Either<Failure, Long> {
-
-            //)
-           //fun sigIn(selectedEmail:String,selectedPass: String) {
-
-               // val readData = FireBaseDataBase().getInstance()
-                // val userFireBase = UserServerModel("",selectedEmail,selectedPass)
-                // val userMap = userFire.createMap()
-                // .addOnSuccessListener(OnSuccessListener<DocumentReference> { /*documentReference ->
-                //                        Log.d(
-                //                            TAG,
-                //                            "DocumentSnapshot added with ID: " + documentReference.id
-                //                        )*/
-                //                    })
-                //                    .addOnFailureListener(OnFailureListener { /*e ->
-                //                        Log.w(
-                //                            TAG,
-                //                            "Error adding document",
-                //                            e
-                //                        )*/
-                //                    })
-                //                //Storing basic user into Local DB
-                //        val localUser = UserDB(id_firebase = "", username = username)
-                //        appDatabase.userDatabaseDao.correctUser(localUser)
-                //        id = appDatabase.userDatabaseDao.correctUser(localUser.username, localUser.password)?.userId
-           // }
-
-         //
-
             val user = UserDB(
                 password = selectedPass,
                 mail = selectedEmail
             )
+
             appDatabase.userDatabaseDao.insert(user)
             val id = appDatabase.userDatabaseDao.correctUser(user.mail,user.password)?.userId
             return if (id!= null) {
                 Either.Right(id)
             } else {
-                Either.Left(Failure.NullResult())
+                Either.Left<Failure.NullResult, Any>(Failure.NullResult())
             }
         }
+        /**
+         * return currentUser from firebaseAuth
+         * */
+        override fun currentUserauth(): FirebaseUser? {
+            return auth.currentUser
+        }
+        /**
+         * use FirebaseAuth method  sigInWithEmailAndPassword,
+         * validates email and pass and then sigIn a user
+         * @email
+         * @pass
+         * */
+        override fun signIn(email: String,pass: String){
+            auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "You´re signin in succesfully ")
+                    //val user=currentUserauth()
 
+                } else {
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
 
+                }
+            }
+        }
+        /**
+         * return active session user
+         * if there not user then return null,
+         * in other cases return sussesful
+         * */
+        override fun checkUser():Either<Failure,Any> {
+            val c = currentUserauth()
+            //user session still active
+            return if (c!= null) {
+                Either.Right(c)
+            } else {
+                Either.Left<Failure.NullResult, Any>(Failure.NullResult())
+            }
+        }
+        /**
+         * use firebase method signOut
+         * to signOut a user
+         * */
+        override fun signOut() {
+           auth.signOut()
+        }
     }
 }
-/*
-* private lateinit var fb
-* */
