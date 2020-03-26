f69b3739c945a619b9eee83b796c3f2a01a78dad
Ruben <rubenizq@ucm.es>
2020-03-26 20:24:32 +0100

    First Ever Test

---
+++ app/src/test/java/com/qflow/main/usecases/user/CreateUserTest.kt
@@ -0,0 +1,110 @@
+package com.qflow.main.usecases.user
+
+import com.qflow.main.repository.UserRepository
+import com.qflow.main.usecases.Either
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.mockito.runners.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class CreateUserTest {
+
+    @Mock
+    private var userRepositoryMock: UserRepository = mock(UserRepository::class.java)
+
+    val createUser: CreateUser = CreateUser(userRepositoryMock)
+
+    private var job = Job()
+    private var coroutineScope = CoroutineScope(Dispatchers.Default + job)
+
+    lateinit var params: CreateUser.Params
+
+    @Before
+    fun setUp() {
+        val username = "TestName"
+        val selectedPass = "TestPass"
+        val selectedRepeatPass = "TestPass"
+        val selectedNameLastName = "TestLast"
+        val selectedEmail = "Test@mail"
+
+        params = CreateUser.Params(
+            username,
+            selectedPass,
+            selectedRepeatPass,
+            selectedNameLastName,
+            selectedEmail
+        )
+    }
+
+    @Test
+    fun runTest_paramsCorrect_na() {
+
+        val username = "TestName"
+        val selectedPass = "TestPass"
+        val selectedNameLastName = "TestLast"
+        val selectedEmail = "Test@mail"
+
+        Mockito.`when`(
+            userRepositoryMock.createUser(
+                username,
+                selectedPass,
+                selectedEmail,
+                selectedNameLastName
+            )
+        ).thenReturn(Either.Right("IdTest"))
+
+        createUser.execute({ it ->
+            it.either({
+                assert(false)
+            }, { s ->
+                assert(s == "IdTest")
+            })
+        }, params, coroutineScope)
+
+
+    }
+
+    @Test
+    fun runTest_paramsIncorrect_goesToError() {
+
+        val username = "TestName"
+        val selectedPass = "TestPass"
+        val selectedRepeatPass = "TestPass1"
+        val selectedNameLastName = "TestLast"
+        val selectedEmail = "Test@mail"
+
+        val params = CreateUser.Params(
+            username,
+            selectedPass,
+            selectedRepeatPass,
+            selectedNameLastName,
+            selectedEmail
+        )
+        Mockito.`when`(
+            userRepositoryMock.createUser(
+                username,
+                selectedPass,
+                selectedEmail,
+                selectedNameLastName
+            )
+        ).thenReturn(Either.Right("IdTest"))
+
+        createUser.execute({
+            it.either(
+                {failure ->
+                assert(true)
+            }, { s ->
+                assert(false)
+            })
+        }, params, coroutineScope)
+    }
+
+
+}
